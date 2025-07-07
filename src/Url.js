64f7bd25b70b0b4a975ab3a36508444b2c12971e
Danielazan <danfrancix@gmail.com>
2025-07-07 18:42:07 +0100

    Basic api change

--- src/Url.js
+++ src/Url.js
@@ -1,7 +1,7 @@
 import { Platform } from 'react-native';
 
-let URL= "http://13.53.88.231:2000"
-
+// let URL= "http://13.53.88.231:2000"
+let URL="https://lazan2.solutionroadequipment.com"
 // let URL
 
 // if (Platform.OS === "android") {
